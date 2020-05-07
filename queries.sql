909086da3a4033692fc1cb3111a2ae58651bb2ea
io mintz <io@mintz.cc>
2020-05-07 04:25:39 +0000

    add license header to queries.sql and fuck mason

--- queries.sql
+++ queries.sql
@@ -1,3 +1,20 @@
+--  © 2020 lambda#0987
+--
+-- Murder Mason is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- Murder Mason is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with Murder Mason. If not, see <https://www.gnu.org/licenses/>.
+
+-- This file must be run in the same database as Emote Collector.
+
 -- :macro update_moderators(batch_size)
 INSERT INTO moderators (id)
 VALUES (
